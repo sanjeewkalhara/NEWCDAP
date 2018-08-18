f789e988afa87234c3aa6bcd1cabbf740afec98e
Pandula Kalhara <pandulakalhara@pandulas-macbook-pro.local>
2018-08-18 20:53:43 +0530

    FirstComiit

---
+++ IT15098856/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  IT15098856
+//
+//  Created by Pandula Kalhara on 8/18/18.
+//  Copyright © 2018 Pandula Kalhara. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
