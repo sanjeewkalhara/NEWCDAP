39a1824341b00975dbe7968aad99525d7bed0bc8
Pandula Kalhara <pandulakalhara@pandulas-macbook-pro.local>
2018-08-18 23:59:53 +0530

    check2

--- IT15098856/Controllers/SecondViewController.swift
+++ IT15098856/Controllers/SecondViewController.swift
@@ -8,18 +8,53 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class SecondViewController: UIViewController,UITextFieldDelegate  {
+    
+    
+    @IBOutlet weak var celsiusTextFeild: UITextField!
+    @IBOutlet weak var kelvinTextFeild: UITextField!
+    @IBOutlet weak var fahrenheitTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        celsiusTextFeild.text=""
+        
+        
+        self.celsiusTextFeild.delegate = self
+        
+        celsiusTextFeild.addTarget(self, action: #selector(textChanged), for: .editingChanged) //identify textfield chaing and call the function textchanged to calculation
+        celsiusTextFeild.clearsOnBeginEditing = true   // clear the current user input if user try to edit textfield
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool { // alow to add numbers only also in storyboard only allow number and function keyboard
+        
+        let allowedCharacters = "+.1234567890"
+        let allowedCharcterSet = CharacterSet(charactersIn: allowedCharacters)
+        let typedCharcterSet = CharacterSet(charactersIn: string)
+        return allowedCharcterSet.isSuperset(of: typedCharcterSet)
+    }
+    
+    @objc func textChanged() {  //calculate the convertion
+        let sum11 = Double(celsiusTextFeild.text!)
+        
+        kelvinTextFeild.text = String(describing:(sum11 ?? 0) + 273.15)
+        
+        fahrenheitTextField.text = String(describing:(sum11 ?? 0 ) * 1.8 + 32)
+        
+        
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {  // function for clear text field
+        return true
+    }
+    
+    
 }
 
